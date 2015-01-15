0850ae585e6b925366621af5bbac8d9176a20ade
Selly <selly@cs.stanford.edu>
2015-01-15 09:31:06 -0800

    Updated to create directories

--- remoteimage.php
+++ remoteimage.php
@@ -1,4 +1,28 @@
 <?php
+function makeDirIfNeeded($path) {
+  if (!file_exists($path)) {
+    mkdir($path, 0777, true);
+  }
+}
+
+if (!isset($_GET['org'])) {
+	exit ('No org id');
+}
+
+$org_id = $_GET['org'];
+
+$con = new mysqli("mysql.climatebase.dreamhosters.com", "climatebase", "climatebas3");
+if ($con->connect_error) {
+	exit ('Connect error (' .mysqli_connect_errno() .') '.mysqli_connect_error());
+} else {
+}
+$con->select_db("climatebase");
+
+// First see if the file is available locally. If not, then download and revise.
+makeDirIfNeeded('./remoteimages/');
+makeDirIfNeeded('./remoteimages/originals/');
+makeDirIfNeeded('./remoteimages/snapshot/');
+
 $handle = fopen('http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/The_Weather_Channel_logo_2005-present.svg/500px-The_Weather_Channel_logo_2005-present.svg.png', 'rb');
 $img = new Imagick();
 $img->readImageFile($handle);
