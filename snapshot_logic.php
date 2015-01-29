e32a8ec5e0a13a09fdda522b01dadedf7bf322a7
Selly <selly@cs.stanford.edu>
2015-01-29 15:31:52 -0800

    switching to AJSON data retrieval

--- snapshot_logic.php
+++ snapshot_logic.php
@@ -1,83 +1,51 @@
-<?php
-include 'database_init.php';
+<div id="snapshot">Snapshot loading... <img src="./img/lightbox/loading.gif"></div>
+<script language="javascript">
+var xmlhttp = new XMLHttpRequest();
+var url = "./snapshot_orgs.php";
+var done = 0;
+var xmlhttp2 = new XMLHttpRequest();
+var url2 = "./snapshot_layout.php";
 
-// PHP code
-function writeOrg($orgName, $logoUrl, $description, $size) {
-  echo "<SPAN TITLE=\"" . $description . "\"><IMG ALT=\"" . $orgName . "\" WIDTH=" . $size . " HEIGHT=" . $size . " SRC='" . $logoUrl . "'></SPAN>";
+function drawSnapshot() {
+  var snapshotDiv = document.getElementById("snapshot");
+  snapshotDiv.innerHTML = "COMPLETE Snapshot is here";
 }
 
-$con = getDBConnection();
-// Create a temporary table with the counts for each cell
-$query = "CREATE TEMPORARY TABLE snapshot_counts AS SELECT org_type, focus, count(1) AS total FROM orgs GROUP BY org_type, focus";
-if (!$con->query($query)) {
-    exit ("Unable to make table");
-}
-if (!$con->query("ALTER TABLE snapshot_counts ADD INDEX (org_type, focus)")) {
-  exit ("Unable to add index");
-}
-$focus_list = array();
-$query = "SELECT focus FROM focus_list ORDER BY focus_order, focus";
-$results = $con->query($query);
-while ($row = $results->fetch_assoc()) {
-  $focus_list[] = $row["focus"];
-}
-$results->free_result();
-echo count($focus_list) . " focus areas<br>";
-$org_type_list = array();
-$query = "SELECT org_type FROM org_type_list ORDER BY org_type_order, org_type";
-$results = $con->query($query);
-while ($row = $results->fetch_assoc()) {
-  $org_type_list[] = $row["org_type"];
-}
-$results->free_result();
-echo count($org_type_list) . " org types<br>";
-echo '<hr>';
-$query = "CREATE TEMPORARY TABLE snapshot_data AS SELECT orgs.*, snapshot_counts.total FROM orgs LEFT JOIN snapshot_counts on (orgs.org_type = snapshot_counts.org_type AND orgs.focus = snapshot_counts.focus) ORDER BY orgs.org_type ASC, orgs.focus ASC";
-if (!$con->query($query)) {
-    exit ("Unable to make table");
-}
-if (!$con->query("ALTER TABLE snapshot_data ADD INDEX (org_type, focus)")) {
-  exit ("Unable to add index");
-}
-$query = "SELECT name, logo_url, description, total FROM snapshot_data WHERE org_type = ? AND focus = ?";
-$stmt = $con->prepare($query);
-echo '<table>';
-foreach($org_type_list as $org_type) {
-  echo '<tr><td>' . $org_type . '</td>';
-  foreach($focus_list as $focus) {
-    echo '<td>';
-    $stmt->bind_param("ss", $org_type, $focus);
-    $count = 0;
-    $stmt->execute();
-    $stmt->bind_result($name, $logo_url, $description, $total);
-    while ($stmt->fetch()) {
-      $count++;
-      $size = 100;
-      $breakpoint = 1;
-      if ($total >= 9) {
-        $size = 50;
-        $breakpoint = 3;
-      } elseif ($total >= 4) {
-        $size = 60;
-        $breakpoint = 2;
-      } elseif ($total > 2) {
-        $size = 100;
-        $breakpoint = 2;
-      }
-      writeOrg($name, $logo_url, $description, $size);
-      if ($count == $breakpoint) {
-        echo '<br>';
-        $count = 0;
-      }
+xmlhttp.onreadystatechange = function() {
+  if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {  
+
+    try {
+      orgs = JSON.parse(xmlhttp.responseText);
+    } catch (e) {
+      alert (e);
+   }
+
+    done++;
+    if (done == 2) {
+      drawSnapshot();
     }
-    echo '</td>';
   }
-  echo '</tr>';
 }
-echo '<tr><td></td>';
-foreach($focus_list as $focus) {
-  echo '<td>' . $focus . '</td>';
+
+xmlhttp2.onreadystatechange = function() {
+  if (xmlhttp2.readyState == 4 && xmlhttp2.status == 200) {
+    try {
+      layout = JSON.parse(xmlhttp2.responseText);
+    } catch (e) {
+      alert(e);
+    }
+    done++;
+    if (done == 2) {
+      drawSnapshot();
+    }
+  }
 }
-echo '</tr></table>'; 
-$con->close();
-?>
+
+window.onload = function() {
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+
+  xmlhttp2.open("GET", url2, true);
+  xmlhttp2.send();
+};
+</script>
