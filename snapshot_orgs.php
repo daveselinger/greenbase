daeeba40967a1855b864cf1931c0d6d2ac315d5c
selly <selly@richrelevance.com>
2015-01-30 09:55:35 -0800

    With real data

--- snapshot_orgs.php
+++ snapshot_orgs.php
@@ -1,15 +1,8 @@
-[
 {
-"display": "JavaScript Tutorial",
-"url": "http://www.w3schools.com/js/default.asp"
-},
-{
-"display": "HTML Tutorial",
-"url": "http://www.w3schools.com/html/default.asp"
-},
-{
-"display": "CSS Tutorial",
-"url": "http://www.w3schools.com/css/default.asp"
+"1": { "id": "1", "name": "Method", "logo": "http://methodhome.com/wp-content/themes/methodhome/images/logo.png",
+  "description": "Method makes home products", "orientation": "square"},
+"2": { "id": "2", "name": "Sungevity", "logo": "http://generation819.com/wp-content/uploads/2014/08/Sungevity-Installations-Logo.jpg",
+"description": "Solar installation", "orientation": "horizontal"},
+"3": { "id": "3", "name": "SunRun", "logo": "http://solaroregon.org/images/sunrun.png/image",
+"description": "Solar manufacturer", "orientation": "square"}
 }
-]
-
