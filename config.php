fc3eedc933dea53af9c1e6caddbd0fe5827e8f81
selly <selly@richrelevance.com>
2015-01-30 17:10:12 -0800

    Database configuration now works.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: selly
+ * Date: 1/29/15
+ * Time: 1:11 PM
+ */
+// VERY IMPORTANT: Localhost leads to using a local socket.
+$db_url='127.0.0.1';
+$db_username='greenbase';
+$db_password='abc';
+$db_name='greenbase';
+?>
--- database_init.php
+++ database_init.php
@@ -9,21 +9,38 @@ if (!isset($db_url) || !isset($db_username) || !isset($db_password) || !isset($d
 	|| ($db_username == '') || ($db_password == '')  || ($db_name == '') ) {
 	exit( 'No database configuration in "config.php"');
 }
+$db_config = [];
+$db_config["url"] = $db_url;
+$db_config["username"] = $db_username;
+$db_config["db"] = $db_name;
+$db_config["password"] = $db_password;
 
-function getDBConnection() {
-$con = new mysqli($db_url, $db_username, $db_password, $db_name);
-if ($con->connect_error) {
-	exit ('Connect error (' .mysqli_connect_errno() .') '.mysqli_connect_error());
-} else {
-}
-$con->select_db("climatebase");
-return $con;
+function getDBConnection($db_config) {
+	$db_url = $db_config["url"];
+	$db_username = $db_config["username"];
+	$db_name = $db_config["db"];
+	$db_password = $db_config["password"];
+
+	if (!isset($db_url) || !isset($db_username) || !isset($db_password) || !isset($db_name) || ($db_url == '')
+		|| ($db_username == '') || ($db_password == '')  || ($db_name == '') ) {
+		exit( 'No database configuration in "config.php"');
+	}
+
+	// exit ("Url: " . $db_url . "; username:" . $db_username . "; password:". $db_password);
+  $con = new mysqli($db_url, $db_username, $db_password);
+	if ($con->connect_error) {
+		exit ('Connect error (' .mysqli_connect_errno() .') '.mysqli_connect_error());
+	} else {
+	}
+	$con->select_db($db_name);
+	return $con;
 }
 
-$con = getDBConnection();
-$results = $con->query("Select count(1) from orgs");
-if ($results->fetch_array()) {
-  echo $results[0] . "rows";
+//Basic test
+$con = getDBConnection($db_config);
+$results = $con->query("Select count(1) as COUNT from orgs");
+if ($row = $results->fetch_assoc()) {
+  echo $row["COUNT"] . "rows";
 }
 $results->close();
 $con->close();
