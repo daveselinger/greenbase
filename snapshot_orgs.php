e22cae37532623d135850c9f18582e3550dab691
selly <selly@richrelevance.com>
2015-02-06 11:42:55 -0800

    JSON object is now driven entirely by the database!

--- snapshot_orgs.php
+++ snapshot_orgs.php
@@ -1,8 +1,30 @@
 {
-"1": { "id": "1", "name": "Method", "logo": "http://methodhome.com/wp-content/themes/methodhome/images/logo.png",
-  "description": "Method makes home products", "orientation": "square"},
-"2": { "id": "2", "name": "Sungevity", "logo": "http://generation819.com/wp-content/uploads/2014/08/Sungevity-Installations-Logo.jpg",
-"description": "Solar installation", "orientation": "horizontal"},
-"3": { "id": "3", "name": "SunRun", "logo": "http://solaroregon.org/images/sunrun.png/image",
-"description": "Solar manufacturer", "orientation": "square"}
+<?php
+include 'database_init.php';
+
+// PHP code
+function writeOrg($orgName, $id, $description, $orientation) {
+  echo("\"$id\": { \"id\": \"$id\", \"name\": \"$orgName\", \"logo\": \"./localimage.php?org=$id\", \"description\": \"$description\", \"orientation\": \"$orientation\"}");
+}
+
+$con = getDBConnection($db_config);
+
+$query = "SELECT orgs.id, name, headline, description, org_type, focus, logo_details.orientation FROM orgs LEFT JOIN logo_details ON (orgs.id = logo_details.id)";
+$results = $con->query($query);
+if (is_null($results)) {
+  exit ("Unable to access orgs");
+}
+
+$first = true;
+while($row = $results->fetch_assoc()) {
+  if (!$first) {
+    echo ",\n";
+  }
+  writeOrg($row["name"], $row["id"], $row["headline"], $row["orientation"]);
+  $first = false;
+}
+$results->free_result();
+$con->close();
+?>
+
 }
