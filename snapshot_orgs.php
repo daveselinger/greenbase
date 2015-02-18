85944b0b08604c28204ac43ec6fa2b010543ae53
selly <selly@richrelevance.com>
2015-02-18 02:03:21 -0800

    Added validity check during JSON generation

--- snapshot_orgs.php
+++ snapshot_orgs.php
@@ -14,8 +14,8 @@ $con = getDBConnection($db_config);
 
 $query = "SELECT orgs.id, name, headline, description, org_type, focus, logo_details.orientation FROM orgs LEFT JOIN logo_details ON (orgs.id = logo_details.id) WHERE orgs.org_status = 1 AND logo_details.valid = 1";
 $results = $con->query($query);
-if (is_null($results)) {
-  exit ("Unable to access orgs");
+if (is_null($results) || false == $results) {
+  exit ("Unable to access orgs:" . $con->error);
 }
 
 $first = true;
