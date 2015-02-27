14b0a05d64c34a9c192e07a1351b7be4b0188b96
selly <selly@richrelevance.com>
2015-02-27 11:41:11 -0800

    Updated to include org_status and logo validity check.

--- organizations_logic.php
+++ organizations_logic.php
@@ -13,14 +13,15 @@ function writeOrg ($id, $orgName, $logoUrl, $description) {
 include 'database_init.php';
 
 $con = getDBConnection($db_config);
-$query = "SELECT id, name, logo_url, description FROM orgs ORDER BY name";
+$query = "SELECT orgs.id, name, orgs.logo_url, description FROM orgs LEFT JOIN logo_details ON (orgs.id = logo_details.id) WHERE org_status = 1 and logo_details.valid = 1 ORDER BY name";
 $results = $con->query($query);
+
 $i=0;
 while ($row = $results->fetch_assoc()) {
   $id = $row["id"];
   $orgName = $row["name"];
   $logoUrl = $row["logo_url"];
-  $logoUrl = './remoteimage.php?org=' . $id;
+  $logoUrl = './localimage.php?org=' . $id . '&width=200';
   $description = $row["description"];
   writeOrg($id, $orgName, $logoUrl, $description);
   $i++;
