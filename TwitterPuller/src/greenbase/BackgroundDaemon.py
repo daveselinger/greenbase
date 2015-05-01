374f61153636b4371576a354483e0d1cc7039d39
Selly <selly@cs.stanford.edu>
2015-04-30 23:17:02 -0700

    Fixed UTF-8 and UNICODE encoding problems with returned tweet text

--- TwitterPuller/src/greenbase/BackgroundDaemon.py
+++ TwitterPuller/src/greenbase/BackgroundDaemon.py
@@ -61,8 +61,15 @@ class BackgroundDaemon (object):
     def getConnection(self):
         for i in [1,2,3,4]:
             try:
-                return MySQLdb.connect(host=self.config.mysqlHost,user=self.config.mysqlUser,
-                                  passwd=self.config.mysqlPassword,db=self.config.mysqlDb)
+                result = MySQLdb.connect(host=self.config.mysqlHost,user=self.config.mysqlUser,
+                                  passwd=self.config.mysqlPassword,db=self.config.mysqlDb,use_unicode=True)
+                result.set_character_set('utf8')
+                dbc = result.cursor()
+                dbc.execute('SET NAMES utf8;')
+                dbc.execute('SET CHARACTER SET utf8;')
+                dbc.execute('SET character_set_connection=utf8;') 
+                return result
+
             except Exception as e:
                 print e
                 print "Exception getting connection, retry {}".format(i)
